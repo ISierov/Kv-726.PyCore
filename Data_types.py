8023b9572e3630894d30509cb1d492f693532069
ISierov <isier@softserveinc.com>
2022-11-22 19:00:35 +0200

    Data Types file added + Zen.txt editing

---
+++ Data_types.py
@@ -0,0 +1,81 @@
+# A = 12
+# a = 15
+# print(A)
+# print(a)
+# frozen = frozenset("qwerty")
+# set1 = set("qwerty")
+# print(frozen)
+# print(set1)
+# dictionary1 = {'name': 'Ivan', 'age': 21, 'role': 'Mentor', 10: 1000}
+# print(dictionary[10], dictionary['name'], sep="\t")
+# x = 20  # immutable
+# y = x
+# print(id(x))
+# x += 10
+# print(id(x))
+#
+# a = [1, 2, 3]  # mutable
+# b = a
+# print(id(a))
+# b += [2, 2, 2]
+# print(id(b))
+
+# a = float(int('100', 2))
+# 0 = 0
+# 1 = 1
+# 10 = 2
+# 11 = 3
+# 100 = 4
+# print(a)
+
+# test = input()
+# print(eval(test))
+# dictionary1 = {'name': 'Ivan', 'age': 21, 'role': 'Mentor', 10: 1000}
+# to_dict = ['name', 'Ivan'], ['age', 21], ['role', 'Mentor'], [10, 1000]
+# dictionary2 = dict(to_dict)
+# print(dictionary1)
+# print(dictionary2)
+# a = []
+# a.append(1)
+# a.append(2)
+# a.append(3)
+# print(a)
+# a ** -2 = 1/(a**2)
+# a**(0.5) -> (0.5 = 1/2) -> sqrt(a)
+# print(16**0.25)  # 16 = 2*2*2*2 = 2**4
+#
+# text1 = 'text'
+# text2 = "text"
+# text3 = """
+# 123451 ->
+# 1245 ->
+# """
+# text4 = '''
+# aefgh
+# '''
+#
+# print(text1, text2, text3, text4, sep="----------")
+
+# name = "John"
+# age = 23
+# print("%s is %d years old." % (name, age))
+#
+# love = "I love Python"
+# print(love.split())
+#
+
+# print('Happy New Year'.find('e'))
+
+# import math
+
+# a = 2.45
+# b = -5.6
+# v = -100.1
+
+# print(math.floor(a), math.trunc(a))
+# print(math.floor(b), math.trunc(b))
+# print(math.floor(v), math.trunc(v))
+
+# print(dir(math))
+
+
