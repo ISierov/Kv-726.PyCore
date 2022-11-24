e8f466da017a97c9994d0b925369d57c9e98277b
ISierov <isier@softserveinc.com>
2022-11-24 18:57:07 +0200

    If statement file added

---
+++ If statements.py
@@ -0,0 +1,21 @@
+e_a = []
+e_d = dict()
+e_se = set()
+e_num = 0
+
+a_e = ['']
+a_d = {"dic": 1234}
+s = '1234'
+a = ['asd']
+
+
+# A or B => False (both empty)
+# print(e_a or e_d)
+# print(s or a)
+
+#print(e_num or a_e or e_se)
+
+
+# A and B => True (not empty)
+# print(e_a and e_d)
+# print(s and a)
