062f351a8df4466871dc167d0696a56967b350b1
ISierov <isier@softserveinc.com>
2022-12-01 19:05:50 +0200

    Collections v.1

---
+++ Collections.py
@@ -0,0 +1,101 @@
+
+arr = [1, 2, 4, 5, 10]
+# print(id(arr))
+# new = arr.copy()
+# arr.append(5)
+# arr.clear()
+# arr.count(1)
+# new.extend([5])
+# n = arr.index(4)
+# arr.insert(3, [5])
+# arr.pop()
+# arr.pop(1)
+# arr.remove(2)
+
+# arr.reverse()
+# arr.sort()
+# new_sort = arr.copy().sort()  # new_sort = sorted(arr)
+# new_rev = reversed(arr)
+# print(id(arr))
+# print(id(new_sort))
+# print(id(new_rev))
+
+# str = list('String')
+# print(str)
+# m = max(arr)
+# m = min(arr)
+# m = sum(arr)
+
+
+# print(m)
+# print(n)
+# print(new)
+#
+# mylist = [True, True, True]
+# x = all(mylist)
+# print(x)
+# ####################
+# mylist = [0, 1, 1]
+# x = all(mylist)
+# print(x)
+# ####################
+# mydict = {4: "Apple", 2: "Orange"}
+# x = all(mydict)
+# print(x)
+
+# mylist = [False, True, False]
+# x = any(mylist)
+# print(x)
+# ####################
+# mytuple = (0, [], False)
+# x = any(mytuple)
+# print(x)
+# ####################
+# mydict = {0 : "Apple", 1 : "Orange"}
+# x = any(mydict)
+# print(x)
+
+# x = ['apple', 'banana', 'cherry']
+# y = enumerate(x, 100)
+# print(list(y))
+
+# new_arr = [i**2 for i in arr if i % 2 == 0]
+# print(arr)
+# print(new_arr)
+
+# a = [1, 2, 3]
+# a1, a2, a3 = a
+#
+# print(a1, a2, a3)
+
+# a = (1, 5, 4)
+# b = reversed(a)
+# print(tuple(b))
+
+
+# s1 = {1, 2, 5}
+# s2 = {2, 5, 7}
+# s3 = s1.difference(s2)
+# s1.difference_update(s2)
+# s1.discard(9)
+# s2.remove(9)
+# s1.intersection_update(s2)
+# s3 = {7, 8, 9}
+# s3 = s1.union(s2)
+# print(s3)
+# print(s1.isdisjoint(s3))
+# print(s1)
+# print(s3)
+
+# s_diff = s1 - s1.intersection(s2)
+# s_diff = s1.difference(s2)
+
+
+# s1 = {(), 1, ''}
+# s3 = {1, 2}
+# print(s1.issubset(s3))
+# print(s3.issubset(s1))  # s1.issuperset(s3)
+# print(s1.issuperset(s3))
+# n = s1.pop()
+# print(s1)
+# print(n)
---
+++ Loops.py
@@ -0,0 +1,30 @@
+
+# start = 0
+# finish = 10
+# while start < finish:
+#     print(start)
+#     start += 1
+# else:
+#     print('End.')
+
+
+# for j in [0, 1, 2, 3, 4]:
+#     print(j)
+# else:
+#     print(j, "is the last")
+
+# for val in "string":
+#     if val == "i":
+#         break
+#     print(val)
+# print("The end")
+#
+# for val in "string":
+#     if val == "i":
+#         continue
+#     print(val)
+# print("The end")
+#
+# for val in 'array':
+#     pass
+
