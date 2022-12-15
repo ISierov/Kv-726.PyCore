924da6eb75d509fa6cccdc76569fc38791532273
ljolia <elena.babko@gmail.com>
2022-12-15 12:09:20 +0200

    weather_json_try.com

--- weather_json_try.py
+++ weather_json_try.py
@@ -1,31 +1,33 @@
 import requests
 import json
+import os
 
-city_is = "kyiv"           # input('My city is: ')
-number_of_days = 2         # input('For how many days ahead you want to see forecast? ')
-apiKey = 'ee230d2c39e18a68e88b6aaa434f6adb'
-response_API = requests.get("https://api.openweathermap.org/data/2.5/forecast?q=kyiv&cnt=2&appid=ee230d2c39e18a68e88b6aaa434f6adb&units=metric")  # apiUrl
-"""
-why this one doesn't work:
-"""
-# response = requests.get("https://api.openweathermap.org/data/2.5/forecast?q={city_is}&cnt={number_of_days}&appid={apiKey}&units=metric")
-# print(response.status_code)
-print(response_API.json())
+CITY = "kyiv"                             # input('My city is: ')
+NUMBER_OF_DAYS = 1                        # input('For how many days ahead you want to see forecast? ')
+API_KEY = 'your_API'
 
-"""can't extract data:"""
-data = response_API.text
-parse_json = json.loads(data)
-weather = parse_json['main']['temp']
-print("temperature is: ", weather)
-"""
-what is wrong? how to extract data like 'temp', 'wind'... ?
-"""
+# print(response_API.json())
+def main():
+    response = requests.get(
+        "https://api.openweathermap.org/data/2.5/forecast",
+        params={"q": CITY, "cnt": NUMBER_OF_DAYS, "appid": API_KEY, "units": "metric"})
+    response.raise_for_status()
+    print("temperature is: ", response.json()['list'][0]['main']['temp'])
+    print("feels_like: ", response.json()['list'][0]['main']['feels_like'])
+    print("pressure: ", response.json()['list'][0]['main']['pressure'])
+    print("humidity: ", response.json()['list'][0]['main']['humidity'])
+    print("weather description: ", response.json()['list'][0]['weather'][0]['description'])
+    print("date: ", response.json()['list'][0]['dt_txt'])
 
-"""
-    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-    document.querySelector("#wind").innerHTML = response.data.wind.speed;
-це з JS, але завдання було таке саме, той самий апі і т.д. Тут зрозуміло, що прописуєш шлях до параметру, який тебе цікавить, як відкриваю коробку і заглиблююсь в наступну папку.
 
-А в python не розумію де дивитись ці данні і як їх вивести.
-Порадьте, як розібратись 
+main()
+"""
+{'cod': '200', 'message': 0, 'cnt': 2,
+'list': [{'dt': 1671105600,
+'main': {'temp': -0.44, 'feels_like': -5.89, 'temp_min': -0.44, 'temp_max': 0.09, 'pressure': 1012, 'sea_level': 1012, 'grnd_level': 996, 'humidity': 94, 'temp_kf': -0.53}, 
+'weather': [{'id': 600, 'main': 'Snow', 'description': 'light snow', 'icon': '13d'}], 'clouds': {'all': 100}, 
+'wind': {'speed': 5.77, 'deg': 178, 'gust': 14.76}, 
+'visibility': 121, 'pop': 1, 'snow': {'3h': 1.3}, 'sys': {'pod': 'd'}, 'dt_txt': '2022-12-15 12:00:00'}], 
+'city': {'id': 703448, 'name': 'Kyiv', 
+'coord': {'lat': 50.4333, 'lon': 30.5167}, 'country': 'UA', 'population': 2514227, 'timezone': 7200, 'sunrise': 1671083509, 'sunset': 1671112454}}
 """
